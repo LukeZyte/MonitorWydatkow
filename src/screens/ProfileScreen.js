eaeaa6930f5b17824fe31356aea32e4b50bedfb7
LukeZyte <lukasz.jarzab01@gmail.com>
2022-09-17 22:46:19 +0200

    working darkMode, fixed styling

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,31 @@
-import { Text } from "react-native";
-import { useTheme } from "@react-navigation/native";
+import { useRef } from "react";
+import { StyleSheet, Animated } from "react-native";
+import { useNavigation, useTheme } from "@react-navigation/native";
+import { useContext, useLayoutEffect } from "react";
+import HeaderButton from "../components/UI/HeaderButton";
+import { ThemeContext } from "../../store/themeContext";
+import TextUI from "../components/UI/TextUI";
+import ToggleThemeIcon from "../components/UI/ToggleThemeIcon";
+import ThemeToggleButton from "../components/UI/ThemeToggleButton";
 
 const ProfileScreen = () => {
   const { colors } = useTheme();
+  const navigation = useNavigation();
+  const { toggleTheme, isDarkTheme } = useContext(ThemeContext);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <ThemeToggleButton />,
+    });
+  }, [navigation]);
 
   return (
     <>
-      <Text>Profile</Text>
+      <TextUI>Profile</TextUI>
     </>
   );
 };
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({});
